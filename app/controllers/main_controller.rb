b5c568e4e76a3134991c560918e9cd64f3a15f5c
James Smith <jgsmith@tamu.edu>
2009-08-31 18:53:57 +0000

    Only get courses for user if there is a user authenticated

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -17,13 +17,22 @@ class MainController < ApplicationController
          :conditions => [ 'starts_at > ?', n ]
       )
 
-      @currently_teaching = @user.courses.select { |c| @current_semesters.include?(c.semester) }
-      @prev_teaching = @user.courses.select { |c| @past_semesters.include?(c.semester) }
-      @will_be_teaching = @user.courses.select { |c| @future_semesters.include?(c.semester) }
+      if @user
+        @currently_teaching = @user.courses.select { |c| @current_semesters.include?(c.semester) }
+        @prev_teaching = @user.courses.select { |c| @past_semesters.include?(c.semester) }
+        @will_be_teaching = @user.courses.select { |c| @future_semesters.include?(c.semester) }
 
-      @currently_taking = @user.course_participants.select { |cp| cp.level == 0 && @current_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
-      @prev_taking = @user.course_participants.select { |cp| cp.level == 0 && @past_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
-      @will_be_taking = @user.course_participants.select { |cp| cp.level == 0 && @future_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
+        @currently_taking = @user.course_participants.select { |cp| cp.level == 0 && @current_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
+        @prev_taking = @user.course_participants.select { |cp| cp.level == 0 && @past_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
+        @will_be_taking = @user.course_participants.select { |cp| cp.level == 0 && @future_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
+      else
+        @currently_teaching = [ ]
+        @prev_teaching = [ ]
+        @will_be_teaching = [ ]
+
+        @currently_taking = [ ]
+        @prev_taking = [ ]
+      end
     end
   end
 end
