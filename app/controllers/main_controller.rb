8b72f296f5ece0a83ca91712c9118d25668dbddc
James Smith <jgsmith@tamu.edu>
2009-08-31 19:03:11 +0000

    Make sure we have a user before we try to get anything from it

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
 
   def index
     @user = current_user
-    if @user
+    if defined? @user && !@user.nil?
       n = Time.now
       @current_semesters = Semester.find( :all,
          :conditions => [ 'starts_at <= ? AND ends_at >= ?', n, n ]
@@ -17,22 +17,24 @@ class MainController < ApplicationController
          :conditions => [ 'starts_at > ?', n ]
       )
 
-      if @user
-        @currently_teaching = @user.courses.select { |c| @current_semesters.include?(c.semester) }
-        @prev_teaching = @user.courses.select { |c| @past_semesters.include?(c.semester) }
-        @will_be_teaching = @user.courses.select { |c| @future_semesters.include?(c.semester) }
+      @currently_teaching = @user.courses.select { |c| @current_semesters.include?(c.semester) }
+      @prev_teaching = @user.courses.select { |c| @past_semesters.include?(c.semester) }
+      @will_be_teaching = @user.courses.select { |c| @future_semesters.include?(c.semester) }
 
-        @currently_taking = @user.course_participants.select { |cp| cp.level == 0 && @current_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
-        @prev_taking = @user.course_participants.select { |cp| cp.level == 0 && @past_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
-        @will_be_taking = @user.course_participants.select { |cp| cp.level == 0 && @future_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
-      else
-        @currently_teaching = [ ]
-        @prev_teaching = [ ]
-        @will_be_teaching = [ ]
+      @currently_taking = @user.course_participants.select { |cp| cp.level == 0 && @current_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
+      @prev_taking = @user.course_participants.select { |cp| cp.level == 0 && @past_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
+      @will_be_taking = @user.course_participants.select { |cp| cp.level == 0 && @future_semesters.include?(cp.course.semester) }.map { |cp| cp.course }
+    else
+      @current_semesters = [ ]
+      @past_semesters = [ ]
+      @future_semesters = [ ]
 
-        @currently_taking = [ ]
-        @prev_taking = [ ]
-      end
+      @currently_teaching = [ ]
+      @prev_teaching = [ ]
+      @will_be_teaching = [ ]
+
+      @currently_taking = [ ]
+      @prev_taking = [ ]
     end
   end
 end
