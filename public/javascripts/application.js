3660eaf4059e43200ea7c44cc82b844967bc7856
James Smith <jgsmith@tamu.edu>
2009-10-01 15:58:53 +0000

    Add blank image url for ExtJS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,6 +2,8 @@
 // This file is automatically included by javascript_include_tag :defaults
 Ext.QuickTips.init();
 
+Ext.BLANK_IMAGE_URL = '/images/s.gif';
+
 Ext.onReady(function(){
 Ext.namespace('WLC');
 
