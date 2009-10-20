ed1346d2f5eebd69b65ede335c5e070df6b7051d
James Smith <jgsmith@tamu.edu>
2009-10-20 15:40:15 +0000

    Round scores to two decimal places

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -17,75 +17,78 @@ class GradesController < ApplicationController
       end
     end
 
-    @assignment.calculate_all_scores unless @assignment.scores_calculated?
     @grades = [ ]
-    @assignment.course.course_participants.each do |cp|
-      next unless cp.is_student?
-      grade = {
-        :id => cp.user.id,
-        :name => cp.user.name,
-      }
+    
+    if @assignment.is_ended?
+      @assignment.calculate_all_scores unless @assignment.scores_calculated?
+      @assignment.course.course_participants.each do |cp|
+        next unless cp.is_student?
+        grade = {
+          :id => cp.user.id,
+          :name => cp.user.name,
+        }
 
-      push_grade = false
-      if @assignment.is_participant?(cp.user)
-        s = @assignment.assignment_submission(cp.user)
-        # we want grades for each component...
-        grade[:is_participant] = true
-        @assignment.configured_modules(nil).each do |m|
-          if m.has_evaluation?
-            s_obj = @assignment.scores.first(:conditions => [
-              'user_id = ? and tag = ?', cp.user.id, m.tag
-            ])
-            if m.participant_rubric
-              i = 0
-              AssignmentParticipation.find(:all,
-                :joins => [ :assignment_submission ],
-                :conditions => [
-                  'assignment_submissions.assignment_id = ? AND
-                   assignment_participations.tag = ? AND
-                   assignment_participations.user_id = ?',
-                  @assignment.id, m.tag, cp.user.id
-                ],
-                :order => 'participant_name').each do |ap|
-                   grade[('participant_' + m.position.to_s + '_' + i.to_s)] =
-                     ap.author_eval ? ap.author_eval_score : '-'
-                   i = i + 1
+        push_grade = false
+        if @assignment.is_participant?(cp.user)
+          s = @assignment.assignment_submission(cp.user)
+          # we want grades for each component...
+          grade[:is_participant] = true
+          @assignment.configured_modules(nil).each do |m|
+            if m.has_evaluation?
+              s_obj = @assignment.scores.first(:conditions => [
+                'user_id = ? and tag = ?', cp.user.id, m.tag
+              ])
+              if m.participant_rubric
+                i = 0
+                AssignmentParticipation.find(:all,
+                  :joins => [ :assignment_submission ],
+                  :conditions => [
+                    'assignment_submissions.assignment_id = ? AND
+                     assignment_participations.tag = ? AND
+                     assignment_participations.user_id = ?',
+                    @assignment.id, m.tag, cp.user.id
+                  ],
+                  :order => 'participant_name').each do |ap|
+                     grade[('participant_' + m.position.to_s + '_' + i.to_s)] =
+                       ap.author_eval ? (ap.author_eval_score*100).round.to_f/100 : '-'
+                     i = i + 1
+                end
+                grade[('participant_' + m.position.to_s + '_avg')] = (s_obj.participant_score*100).round.to_f/100 unless s_obj.participant_score.nil?
               end
-              grade[('participant_' + m.position.to_s + '_avg')] = s_obj.participant_score unless s_obj.participant_score.nil?
-            end
-            if m.author_rubric
-              i = 0
-              AssignmentParticipation.find(:all,
-                :conditions => [
-                  'assignment_submission_id = ? AND
-                   tag = ?',
-                  s.id, m.tag
-                ],
-                :order => 'author_name').each do |ap|
-                  grade[('author_' + m.position.to_s + '_' + i.to_s)] =
-                    ap.participant_eval ? ap.participant_eval_score : '-'
-                  i = i + 1
+              if m.author_rubric
+                i = 0
+                AssignmentParticipation.find(:all,
+                  :conditions => [
+                    'assignment_submission_id = ? AND
+                     tag = ?',
+                    s.id, m.tag
+                  ],
+                  :order => 'author_name').each do |ap|
+                    grade[('author_' + m.position.to_s + '_' + i.to_s)] =
+                      ap.participant_eval ? (ap.participant_eval_score*100).round.to_f/100 : '-'
+                    i = i + 1
+                end
+                grade[('author_' + m.position.to_s + '_avg')] = (s_obj.author_score*100).round.to_f/100 unless s_obj.author_score.nil?
               end
-              grade[('author_' + m.position.to_s + '_avg')] = s_obj.author_score unless s_obj.author_score.nil?
             end
           end
-        end
 
-        grade[:self_eval] = s.author_eval_score
-        grade[:final] = s.score
-        if s.trust.nil?
-          grade[:trust] = '-'
+          grade[:self_eval] = (s.author_eval_score*100).round.to_f/100
+          grade[:final] = (s.score*100).round.to_f/100
+          if s.trust.nil?
+            grade[:trust] = '-'
+          else
+            grade[:trust] = (s.trust * 100.0 * 100).round.to_f/100
+          end
         else
-          grade[:trust] = s.trust * 100.0
+          grade[:is_participant] = false
         end
-      else
-        grade[:is_participant] = false
-      end
 
-      @grades << grade
+        @grades << grade
+      end
     end
     respond_to do |format|
-      format.html
+      #format.html
       format.ext_json { render :json => { :results => @grades.length, :items => @grades } }
     end
   end
