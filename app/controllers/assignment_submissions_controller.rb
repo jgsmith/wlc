6ad02c232f5665197230bd32f48eea8064d0e552
James Smith <jgsmith@tamu.edu>
2009-11-04 16:15:41 +0000

    Factor out grade calculation/presentation prep

--- app/controllers/assignment_submissions_controller.rb
+++ app/controllers/assignment_submissions_controller.rb
@@ -12,63 +12,11 @@ class AssignmentSubmissionsController < ApplicationController
   def index
     @user = current_user
     if @assignment.course.is_assistant?(@user)
-      # dump all grades for all participants
-      @grades = [ ]
-      @assignment.course.course_participants.each do |student|
-        next if student.level > 0
-        submission = AssignmentSubmission.find(:first, :conditions => [
-          'assignment_id = ? AND user_id = ?',
-          @assignment.id, student.user.id ]) rescue nil
-
-        grade = { :id => student.user.id, :name => student.user.name }
-        
-        if submission
-          grade[:is_participant] = true
-          grade[:progress_info] = submission.show_info(@assignment.current_module(nil) ? @assignment.current_module(nil).position + 1 : @assignment.configured_modules(nil).last.position + 1)
-          grade[:messages_url] = assignment_submission_messages_path(submission)
-
-          @assignment.configured_modules(nil).each do |m|
-            if m.has_evaluation?
-              if !m.author_name.blank?
-                i = 0
-                AssignmentParticipation.find(:all,
-                  :joins => [ :assignment_submission ],
-                  :conditions => [
-                    'assignment_submissions.assignment_id = ? AND
-                     assignment_participations.tag = ? AND
-                     assignment_participations.user_id = ?',
-                    @assignment.id, m.tag, student.user.id
-                  ],
-                  :order => 'author_name').each do |ap|
-                     grade[('author_' + m.position.to_s + '_' + i.to_s)] =
-                       ap.assignment_submission.user.name
-                     i = i + 1
-                end
-              end
-              if !m.participant_name.blank?
-                i = 0
-                AssignmentParticipation.find(:all,
-                  :conditions => [
-                    'assignment_submission_id = ? AND
-                     tag = ?',
-                    submission.id, m.tag
-                  ],
-                  :order => 'participant_name').each do |ap|
-                    grade[('participant_' + m.position.to_s + '_' + i.to_s)] =
-                      ap.user.name
-                    i = i + 1
-                end
-              end
-            end
-          end
-        else
-          grade[:is_participant] = false
-          grade[:progress_info] = '<p>No submission for this assignment.</p>'
-        end
-        @grades << grade
-      end
       respond_to do |format|
-        format.ext_json { render :json => { :results => @grades.length, :items => @grades } }
+        format.html { prep_for_index_html }
+        format.ext_json { prep_for_index_json
+          render :json => { :results => @grades.length, :items => @grades } 
+        }
       end
     elsif @assignment.course.is_student?(@user)
       # dump grades for the student
@@ -83,6 +31,100 @@ class AssignmentSubmissionsController < ApplicationController
   end
 
 protected
+
+  def prep_for_index_html
+    @configured_modules_info = [ ]
+    @performance_store_fields = [ 'id', 'name', 'is_participant', 'progress_info',  'messages_url' ]
+    @performance_columns = [
+      { :id => 'name', :dataIndex => 'name', :header => 'Name', :sortable => true }
+    ]
+
+    @assignment.configured_modules(nil).each do |m|
+      if m.has_evaluation?
+        if !m.author_name.blank?
+          m.number_participants.times do |i|
+            nm = 'author_' + m.position.to_s + '_' + i.to_s
+            @performance_store_fields << nm
+            @performance_columns << {
+              :header => m.author_name + ' #' + (i+1).to_s,
+              :dataIndex => nm,
+              :sortable => true
+            }
+          end
+        end
+        if !m.participant_name.blank?
+          m.number_participants.times do |i|
+            nm = 'participant_' + m.position.to_s + '_' + i.to_s
+            @performance_store_fields << nm
+            @performance_columns << {
+              :header => m.participant_name + ' #' + (i+1).to_s,
+              :dataIndex => nm,
+              :sortable => true
+            }
+          end
+        end
+      end
+    end
+  end
+
+  def prep_for_index_json
+    # dump all grades for all participants
+
+    @grades = [ ]
+    @assignment.course.course_participants.each do |student|
+      next if student.level > 0
+      submission = AssignmentSubmission.find(:first, :conditions => [
+        'assignment_id = ? AND user_id = ?',
+        @assignment.id, student.user.id ]) rescue nil
+
+      grade = { :id => student.user.id, :name => student.user.name }
+  
+      if submission
+        grade[:is_participant] = true
+        grade[:progress_info] = submission.show_info(@assignment.current_module(nil) ? @assignment.current_module(nil).position + 1 : @assignment.configured_modules(nil).last.position + 1)
+        grade[:messages_url] = assignment_submission_messages_path(submission)
+
+        @assignment.configured_modules(nil).each do |m|
+          if m.has_evaluation?
+            if !m.author_name.blank?
+              i = 0
+              AssignmentParticipation.find(:all,
+                :joins => [ :assignment_submission ],
+                :conditions => [
+                  'assignment_submissions.assignment_id = ? AND
+                   assignment_participations.tag = ? AND
+                   assignment_participations.user_id = ?',
+                  @assignment.id, m.tag, student.user.id
+                ],
+                :order => 'author_name').each do |ap|
+                   grade[('author_' + m.position.to_s + '_' + i.to_s)] =
+                     ap.assignment_submission.user.name
+                   i = i + 1
+              end
+            end
+            if !m.participant_name.blank?
+              i = 0
+              AssignmentParticipation.find(:all,
+                :conditions => [
+                  'assignment_submission_id = ? AND
+                   tag = ?',
+                  submission.id, m.tag
+                ],
+                :order => 'participant_name').each do |ap|
+                  grade[('participant_' + m.position.to_s + '_' + i.to_s)] =
+                    ap.user.name
+                  i = i + 1
+              end
+            end
+          end
+        end
+      else
+        grade[:is_participant] = false
+        grade[:progress_info] = '<p>No submission for this assignment.</p>'
+      end
+      @grades << grade
+    end
+  end
 
   def find_assignment
     @assignment = Assignment.find(params[:assignment_id])
