83674dc8e40d0d856be625ce7d04b54a5e7f15bd
James Smith <jgsmith@tamu.edu>
2009-11-04 16:14:44 +0000

    Pull out list of current assignments for all requests (used in layout); set up cas auth for future modifications to make it optional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   include AuthenticatedSystem
   include ExtScaffold
 
-  before_filter CASClient::Frameworks::Rails::Filter
+  before_filter :cas_auth
   before_filter :set_users
 
   helper :all # include all helpers, all the time
@@ -17,5 +17,40 @@ class ApplicationController < ActionController::Base
   def set_users
     @actual_user = actual_user
     @user = current_user
+
+    @current_assignments = [ ]
+    if @user
+      current_courses = @user.course_participants.find(:all,
+        :joins => %{
+          LEFT JOIN courses ON courses.id = course_participants.course_id
+          LEFT JOIN semesters ON semesters.id = courses.semester_id
+        },
+        :conditions => [
+          %{
+            semesters.utc_starts_at <= ? AND
+            semesters.utc_ends_at >= ?
+          }, Time.now(), Time.now()
+        ]
+      ).collect{|cp| cp.course} + @user.courses.find(:all,
+        :joins => 'LEFT JOIN semesters ON semesters.id = courses.semester_id',
+        :conditions => [
+          %{
+            semesters.utc_starts_at <= ? AND
+            semesters.utc_ends_at >= ?
+          }, Time.now(), Time.now()
+        ]
+      )
+      @current_assignments = current_courses.select{|c|
+        c.has_current_assignment?
+      }
+    end
+  end
+
+  def self.no_auth_required
+    CASClient::Frameworks::Rails::Filter.use_gatewaying(self)
+  end
+
+  def cas_auth
+    CASClient::Frameworks::Rails::Filter.filter(self)
   end
 end
